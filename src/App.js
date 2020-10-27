2948f5bc1f0fb2fbc858b08fffa7789cacd6b05d
Bruno <Brunodornelesdeabreu@hotmail.com>
2020-10-26 21:08:07 -0300

    index

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import "./components/menu/MenuLateral";
 import MenuLateral from "./components/menu/MenuLateral";
 import BarraCima from "./components/header/BarraCima";
 import Menucadastrocliente from "./components/menu/Menucadastrocliente";
+import Status from "./components/Notificação/Status";
 
 function App() {
   return (
     <div >
       <BarraCima></BarraCima>
+      <div className="Noti">
       <MenuLateral></MenuLateral>
+      <Status></Status>
+      </div>
     </div>
   );
 }
---
+++ src/components/Notificação/Status.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/style.css";
+
+function Status() {
+  return (
+    <section>
+      <div class="item">Serviços em andamentos</div>
+    </section>
+  );
+}
+export default Status;
