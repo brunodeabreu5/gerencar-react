b25c11a424ee2c96879ce862074960f2d8ffa1f0
Bruno <Brunodornelesdeabreu@hotmail.com>
2020-10-27 22:43:38 -0300

    m

--- src/components/Notificacao/ItemStatus/index.jsx
+++ src/components/Notificacao/ItemStatus/index.jsx
@@ -21,6 +21,34 @@ function ItemStatus(){
             <div className="statusPlaca">MNL-8385</div>
             <div className="statusEntrada">14/09/2020</div>
           </div>
+          <div className="statusveiculos">
+          <div className="statusid">2</div>
+          <div className="statusStatus">Em <br></br>Andamento</div>
+            <div className="statusfuncionario">Rodrigo</div>
+            <div className="statusPlaca">MNL-8385</div>
+            <div className="statusEntrada">14/09/2020</div>
+          </div>
+          <div className="statusveiculos">
+          <div className="statusid">2</div>
+          <div className="statusStatus">Em <br></br>Andamento</div>
+            <div className="statusfuncionario">Rodrigo</div>
+            <div className="statusPlaca">MNL-8385</div>
+            <div className="statusEntrada">14/09/2020</div>
+          </div>
+          <div className="statusveiculos">
+          <div className="statusid">2</div>
+          <div className="statusStatus">Em <br></br>Andamento</div>
+            <div className="statusfuncionario">Rodrigo</div>
+            <div className="statusPlaca">MNL-8385</div>
+            <div className="statusEntrada">14/09/2020</div>
+          </div>
+          <div className="statusveiculos">
+          <div className="statusid">2</div>
+          <div className="statusStatus">Em <br></br>Andamento</div>
+            <div className="statusfuncionario">Rodrigo</div>
+            <div className="statusPlaca">MNL-8385</div>
+            <div className="statusEntrada">14/09/2020</div>
+          </div>
         </div>
         
     );
