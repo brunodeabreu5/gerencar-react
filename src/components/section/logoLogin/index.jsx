0ab43f75061eaaee9202ac69379bfc18e8412ed5
Bruno <Brunodornelesdeabreu@hotmail.com>
2020-11-10 22:29:52 -0300

    tela login

--- src/components/section/logoLogin/index.jsx
+++ src/components/section/logoLogin/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./index.css";
-import logoLogin from "";
+import logo from "../../../Assets/CARRO-preto.png";
 
 function logoLogin(){
     return(
-
+        <div>
+        <a className="fundoLogin"><img src={logo} alt="Logo do Gerencar"/></a>
+       
+        </div>
     );
 }
+
+export default logoLogin;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../styles/style.css";
 import "./index.css";
-
+import LogoLogin from "../../components/section/logoLogin/index";
 
 function Login() {
   return (
     <div className="fundoLogin">
       <div className="frame">
-
+      <LogoLogin/>
+      <h1>Gerencar</h1>
       </div>
     </div>
   );
