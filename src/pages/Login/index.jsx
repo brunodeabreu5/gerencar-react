20cd437012e11ecfb7a08cba0ba59d2a9c930b67
Bruno <brunodornelesdeabreu@hotmail.com>
2020-12-03 22:41:50 -0300

    login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,46 +1,60 @@
-import React, { useState } from "react";
-import "../../styles/style.css";
-import axios from "axios";
-import "./index.css";
-import LogoLogin from "../../components/section/logoLogin/index";
-import FormLogin from "../../components/Form/FormLogin/index";
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } from 'mdbreact';
 
-const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState();
-
-  const handleSubmit = async (e) => {};
-
-  if (user) {
-    return <div>{user.name} is loggged in</div>;
-  }
-
+function Login () {
   return (
-    <div className="fundoFrame">
-    <form className="frame" onSubmit={handleSubmit}>
-      <LogoLogin size="10px" />
-      <h1>Gerencar</h1>
-      <div><label htmlFor="username">Usuario: </label>
-      <input
-        type="text"
-        value={username}
-        placeholder="enter a username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-      </div>
-      <div>
-        <label htmlFor="password">Senha: </label>
-        <input
-          type="password"
-          value={password}
-          placeholder="enter a password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-    </div>
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="6">
+          <MDBCard>
+            <div className="header pt-3 grey lighten-2">
+              <MDBRow className="d-flex justify-content-start">
+                <h3 className="deep-grey-text mt-3 mb-4 pb-1 mx-5">
+                  Log in
+                </h3>
+              </MDBRow>
+            </div>
+            <MDBCardBody className="mx-4 mt-4">
+              <MDBInput label="Your email" group type="text" validate />
+              <MDBInput
+                label="Your password"
+                group
+                type="password"
+                validate
+                containerClass="mb-0"
+              />
+              <p className="font-small grey-text d-flex justify-content-end">
+                Forgot
+                <a
+                  href="#!"
+                  className="dark-grey-text font-weight-bold ml-1"
+                >
+                  Password?
+                </a>
+              </p>
+              <div className="text-center mb-4 mt-5">
+                <MDBBtn
+                  color="danger"
+                  type="button"
+                  className="btn-block z-depth-2"
+                >
+                  Log in
+                </MDBBtn>
+              </div>
+              <p className="font-small grey-text d-flex justify-content-center">
+                Don't have an account?
+                <a
+                  href="#!"
+                  className="dark-grey-text font-weight-bold ml-1"
+                >
+                  Sign up
+                </a>
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
